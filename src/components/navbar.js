1848cdb11b854f53e32435b3fd33aafa8ccd815d
davit.gevorgyan <davit.gevorgyan@algorithm.am>
2020-02-14 14:23:04 +0400

    Update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,16 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-const Wrapper = styled.nav`
-  background-color: #000;
-  width: 100%;
-  height: 90px;
-  display: flex;
-  flex-direction: row;
-`;
-
 const NavLink = styled(Link)`
   flex-grow: 1;
+  flex-basis: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -21,31 +14,39 @@ const NavLink = styled(Link)`
   text-decoration: none;
 `;
 
-const Side = styled.div`
-  flex-grow: 2;
-  display: flex;
-  flex-direction: column;
+const GridWrapper = styled.nav`
+  padding: 1px;
+  background-color: #ebebeb;
+  width: 100%;
+  height: 100px;
+  display: grid;
+  grid-template-areas:
+    'lang addr addr bla1 bla1 bla1 bla1 bla1 bla1 home home home home bla2 bla2 bla2 bla2 bla2 bla2 phon phon phon'
+    'serv serv serv repo repo repo calc calc calc home home home home info info info join join join cont cont cont';
+  grid-template-rows: 8fr 18fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 1px;
+  > div {
+    background-color: #fff;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
-const LeftTop = styled.div`
-  height: 20px;
-  flex-grow: 1;
-`;
-
-const LeftBottom = styled.div`
-  flex-grow: 3;
-  display: flex;
-`;
-
-const RightTop = styled.div`
-  height: 20px;
-  flex-grow: 1;
-`;
-
-const RightBottom = styled.div`
-  flex-grow: 3;
-  display: flex;
-`;
+const GridLang = styled.div`grid-area: lang;`;
+const GridAddress = styled.div`grid-area: addr;`;
+const GridBlank1 = styled.div`grid-area: bla1;`;
+const GridHome = styled.div`grid-area: home`;
+const GridBlank2 = styled.div`grid-area: bla2;`;
+const GridPhone = styled.div`grid-area: phon;`;
+const GridService = styled.div`grid-area: serv;`;
+const GridReport = styled.div`grid-area: repo;`;
+const GridCalc = styled.div`grid-area: calc;`;
+const GridInfo = styled.div`grid-area: info;`;
+const GridJoin = styled.div`grid-area: join;`;
+const GridContact = styled.div`grid-area: cont;`;
 
 const activeStyle = {
   color: '#009db8',
@@ -53,39 +54,56 @@ const activeStyle = {
 
 const Navbar = () => {
   return (
-    <Wrapper>
-      <Side>
-        <LeftTop />
-        <LeftBottom>
+    <>
+      <GridWrapper>
+        <GridLang>
+          <select id="lang">
+            <option value="hy">Հայ</option>
+            <option value="en">Eng</option>
+            <option value="ru">Рус</option>
+          </select>
+        </GridLang>
+        <GridAddress>Հր Քոչար 44</GridAddress>
+        <GridBlank1 />
+        <GridHome>
+          <NavLink to="/" activeStyle={activeStyle}>
+            Triple Consulting
+          </NavLink>
+        </GridHome>
+        <GridBlank2 />
+        <GridPhone>374 93 00 00 00</GridPhone>
+        <GridService>
           <NavLink to="/services/" activeStyle={activeStyle}>
             Ծառայություններ
           </NavLink>
+        </GridService>
+        <GridReport>
           <NavLink to="/reports/" activeStyle={activeStyle}>
             Հաշվետվության<br /> տրամադրում
           </NavLink>
+        </GridReport>
+        <GridCalc>
           <NavLink to="/calculator/" activeStyle={activeStyle}>
             Հաշվիչ
           </NavLink>
-        </LeftBottom>
-      </Side>
-      <NavLink to="/" activeStyle={activeStyle}>
-        Home
-      </NavLink>
-      <Side>
-        <RightTop />
-        <RightBottom>
+        </GridCalc>
+        <GridInfo>
           <NavLink to="/information/" activeStyle={activeStyle}>
             Օգտակար<br /> տեղեկություն
           </NavLink>
+        </GridInfo>
+        <GridJoin>
           <NavLink to="/career/" activeStyle={activeStyle}>
             Միացիր<br /> մեր թիմին
           </NavLink>
+        </GridJoin>
+        <GridContact>
           <NavLink to="/contact/" activeStyle={activeStyle}>
             Կապ մեզ հետ
           </NavLink>
-        </RightBottom>
-      </Side>
-    </Wrapper>
+        </GridContact>
+      </GridWrapper>
+    </>
   );
 };
 
