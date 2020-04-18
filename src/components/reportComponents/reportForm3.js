93bc9f761a01871185de7dd8f3f4b1ad379a4dee
Tagayan061994 <htagayan@gmail.com>
2020-04-18 17:39:15 +0400

    report responsive version 1.0.0

--- src/components/reportComponents/reportForm3.js
+++ src/components/reportComponents/reportForm3.js
@@ -6,7 +6,7 @@ import {
 } from "@ant-design/icons"
 import styled from "styled-components"
 import { Form, Button, Upload, Row, Col } from "antd"
-import UploadImage from '../../assets/upload2.svg'
+import UploadImage from "../../assets/upload2.svg"
 import PropTypes from "prop-types"
 
 const formItemLayout = {
@@ -23,8 +23,8 @@ const normFile = e => {
 }
 
 const UploadWrapper = styled.img`
-    width:20px;
-    height:20px;
+  width: 20px;
+  height: 20px;
 `
 const CustomButton = styled(Button)`
   width: 448px;
@@ -34,6 +34,55 @@ const CustomButton = styled(Button)`
   padding-top: 1.7%;
   display: flex;
   justify-content: space-between;
+  span {
+      width: 208px;
+      height: 31px;
+      font-family: ArialAMU;
+      font-size: 14px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      text-align: left;
+      color: #000000;
+    }
+  @media (min-width: 320px) {
+    width: 290px;
+    border: solid 1px #009db8;
+    padding-top: 3.7%;
+    span {
+      width: 208px;
+      height: 31px;
+      font-family: ArialAMU;
+      font-size: 14px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      text-align: left;
+      color: #000000;
+    }
+  }
+  @media (min-width: 375px) {
+    width: 290px;
+    border: solid 1px #009db8;
+    padding-top: 3.7%;
+    span {
+      width: 208px;
+      height: 41px;
+      font-family: ArialAMU;
+      font-size: 11px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      text-align: left;
+      color: #000000;
+    }
+  }
 `
 
 const H6Styled = styled.h6`
@@ -49,10 +98,74 @@ const H6Styled = styled.h6`
   text-align: left;
   color: #000000;
 `
-
-function ReportForm3(props) {
+const NavigateWrapper = styled.div`
+  width: 180px;
+  display: flex;
+  justify-content: space-between;
+`
+const NavigateBackButton = styled(Button)`
+  width: 90px;
+  height: 20px;
+  border: solid 0px;
+  display: flex;
+  padding-left: 0;
+  justify-content: space-between;
+  background-color: rgba(255, 0, 0, 0);
+  &:hover {
+    background-color: rgba(255, 0, 0, 0);
+    cursor: pointer;
+  }
+`
+const NavigateForwardButton = styled(Button)`
+  width: 90px;
+  height: 20px;
+  border: solid 0px;
+  display: flex;
+  padding-right: 0;
+  justify-content: space-between;
+  background-color: rgba(255, 0, 0, 0);
+  &:hover {
+    background-color: rgba(255, 0, 0, 0);
+    cursor: pointer;
+  }
+`
+const BackSpan = styled.span`
+  height: 12px;
+  font-family: ArialAMU;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #009db8;
+`
+const ForwardSpan = styled.span`
+  height: 12px;
+  font-family: ArialAMU;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #009db8;
+`
+function ReportForm3({ forwardButton, backButton }) {
     const onFinish = values => {
         console.log("Received values of form: ", values)
+        setConfirm2(false)
+        setConfirm3(true)
+    }
+
+    const goBack = () => {
+        backButton(true)
+    }
+    const goForward = () => {
+        setConfirm2(false)
+        setConfirm3(true)
     }
     return (
         <Row gutter={[10, 25]}>
@@ -78,7 +191,8 @@ function ReportForm3(props) {
                     >
                         <Upload name="passport" action="/upload.do" listType="picture">
                             <CustomButton>
-                                <span>Անձնագիր</span><UploadWrapper src={UploadImage} />
+                                <span>Անձնագիր</span>
+                                <UploadWrapper src={UploadImage} />
                             </CustomButton>
                         </Upload>
                     </Form.Item>
@@ -90,7 +204,8 @@ function ReportForm3(props) {
                     >
                         <Upload name="carconract" action="/upload.do" listType="picture">
                             <CustomButton>
-                                <span>Ավտոմեքենայի առք ու վաճառքի պայմանագիր</span><UploadWrapper src={UploadImage} />
+                                <span>Ավտոմեքենայի առք ու վաճառքի պայմանագիր</span>
+                                <UploadWrapper src={UploadImage} />
                             </CustomButton>
                         </Upload>
                     </Form.Item>
@@ -102,17 +217,32 @@ function ReportForm3(props) {
                     >
                         <Upload name="authorize" action="/upload.do" listType="picture">
                             <CustomButton>
-                                <span>Լիազորագիր</span><UploadWrapper src={UploadImage} />
+                                <span>Լիազորագիր</span>
+                                <UploadWrapper src={UploadImage} />
                             </CustomButton>
                         </Upload>
                     </Form.Item>
 
                     <Form.Item wrapperCol={{ span: 12, offset: 0 }}>
-                        <Button type="primary" htmlType="submit" id="registerSubmit" >
+                        <Button type="primary" htmlType="submit" id="registerSubmit">
                             Հաստատել
-                        </Button>
+            </Button>
                     </Form.Item>
                 </Form>
+            </Col>
+            <Col span={24}>
+                <NavigateWrapper>
+                    <NavigateBackButton onClick={goBack}>
+                        <ArrowLeftOutlined style={{ color: "#009db8", fontSize: "15px" }} />
+                        <BackSpan>Հետ</BackSpan>
+                    </NavigateBackButton>
+                    <NavigateForwardButton onClick={goForward}>
+                        <ForwardSpan>Առաջ</ForwardSpan>
+                        <ArrowRightOutlined
+                            style={{ color: "#009db8", fontSize: "15px" }}
+                        />
+                    </NavigateForwardButton>
+                </NavigateWrapper>
             </Col>
         </Row>
     )
